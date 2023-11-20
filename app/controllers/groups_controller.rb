56cd6a3960a8bf87c4a5df4209300b15bdcbf9ab
agneta1991 <pupienytea@gmail.com>
2023-11-20 18:25:57 +0200

    categories new page sett up

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,22 +6,23 @@ class GroupsController < ApplicationController
   end
 
   def new
-    @groups = Group.all
+    @group = Group.new
   end
 
+
   def create
-    @group = Group.new(group_params)
-  
+    @group = Group.new(group_params.merge(user_id: current_user.id))
+
     if @group.save
-      redirect_to @group, notice: 'Group was successfully created.'
+      redirect_to groups_path, notice: 'Group was successfully created.'
     else
-      render :new
+      render :new, status: 422
     end
   end
 
   def destroy; end
 
   def group_params
-    params.require(:group).permit(:name, :icon)
+    params.require(:group).permit(:name, :user_id, :icon)
   end
 end
